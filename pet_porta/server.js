c876f112b31bdf6272f7d616b686b8277f7ddd62
hpbonfim <hp_bonfim@hotmail.com>
2018-10-31 08:44:21 -0400

    fix

--- pet_porta/server.js
+++ pet_porta/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 80
 const router = express.Router()
 
 app.use('/', express.static(`${__dirname}/dist`)) // set the static files location for the static html
