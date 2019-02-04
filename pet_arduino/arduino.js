3cb67fb97c314d4b37456bb88e7fea6de8426ee3
hpbonfim <hp_bonfim@hotmail.com>
2019-02-04 19:10:12 -0300

    latest v0.09

--- pet_arduino/arduino.js
+++ pet_arduino/arduino.js
@@ -1,88 +1,67 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const port = 3333;
+const port = 3333
 const bodyParser = require('body-parser')
-let setState
-// var EthernetPort = require('etherport')
+const cors = require('cors')
+const EtherPort = require("etherport")
 const five = require('johnny-five')
-const board = new five.Board() // { port: new EthernetPort(3030) }
-
+const songs = require('j5-songs')
+const board = new five.Board({ repl: false })//port: new EtherPort(3030)
+let buzzer, melody
+var relay
+ // CORS Middleware
 app.use(cors())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
+
+app.use(express.static(__dirname))
+
 //Test
-app.get('/', function (req, res){
+app.get('/test',(req, res) => {
   console.log('Online')
+  board.on("ready", function() {
+    buzzer = new five.Piezo({
+      type: 'NC',
+      pin: 4
+    })
+    melody = songs.load("mario-intro")
+    console.log('play:', melody)
+    buzzer.play(melody)
+    res.sendStatus(200)
+  })
 })
-//Toogle
-app.get('/toogle', function(req, res){
-  setState.toogle()
-  res.redirect('/')
+
+app.get('/time', (req, res) => {
+  const time = (new Date()).toLocaleTimeString()
+  res.status(404).send(`Hora: ${time}`)
 })
-//Piezo Teste
-board.on("ready", function() {
-  // Creates a piezo object and defines the pin to be used for the signal
-  var piezo = new five.Piezo(3);
 
-  // Injects the piezo into the repl
-  board.repl.inject({
-    piezo: piezo
-  });
+app.get('/abrir',(req, res) => {
+  global.off()  
+  setTimeout(function(){
+    console.log('abriu')    
+    global.on()
+      }, 1000)
+  res.sendStatus(200)
+})
 
-  // Plays a song
-  piezo.play({
-    // song is composed by an array of pairs of notes and beats
-    // The first argument is the note (null means "no note")
-    // The second argument is the length of time (beat) of the note (or non-note)
-    song: [
-      ["C4", 1 / 4],
-      ["D4", 1 / 4],
-      ["F4", 1 / 4],
-      ["D4", 1 / 4],
-      ["A4", 1 / 4],
-      [null, 1 / 4],
-      ["A4", 1],
-      ["G4", 1],
-      [null, 1 / 2],
-      ["C4", 1 / 4],
-      ["D4", 1 / 4],
-      ["F4", 1 / 4],
-      ["D4", 1 / 4],
-      ["G4", 1 / 4],
-      [null, 1 / 4],
-      ["G4", 1],
-      ["F4", 1],
-      [null, 1 / 2]
-    ],
-    tempo: 100
-  });
-
-  // Plays the same song with a string representation
-  piezo.play({
-    // song is composed by a string of notes
-    // a default beat is set, and the default octave is used
-    // any invalid note is read as "no note"
-    song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
-    beats: 1 / 4,
-    tempo: 100
-  });
-
-});
+app.get('/on', function(req, res) {
+  board.on('ready', function() {
+    var relay = new five.Relay(10)
+    board.repl.inject({
+      relay: relay
+    })
+    relay.toggle()
+    return res.sendStatus(200)
+  })    
+})
 
 // ------------------------------------------------------------
-board.on('ready', function () {
-  console.log('Arduino está pronto!')
-  relay = new five.Relay({
-    type: 'NC',
-    pin: 10
-  })
-    setState = relay
+
+app.listen(port, function() {
+  console.log("Arduino port: ",port);
 })
 
 // ----------------------------------------------------
-
-app.listen(port)
-console.log("Arduino port: ",port)
