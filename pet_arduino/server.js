6ecd934f27da8403dd352318cd38b3188c1b9eae
hpbonfim <hpbonfim>
2019-03-02 21:33:23 -0400

    latest

--- pet_arduino/server.js
+++ pet_arduino/server.js
@@ -9,41 +9,33 @@ const songs = require('j5-songs')
 const five = require("johnny-five")
 const server = http.createServer(app)
 const EtherPort = require("etherport")
-let board, error
-board = new five.Board({repl: false/*port: new EtherPort(3030)*/})
-
 //---------------------------------------------//
 app.use(cors()) // simple CORS Middleware
 app.use(bodyParser.urlencoded({ extended: true })) // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse requests of content-type - application/json
 
 //------------------------------------------ Arduino Modules
-function arduino() { 
-  return new Promise((resolve, reject) => {
+async function arduino() { 
+  return await new Promise((resolve, reject) => {
+      let board
+      setTimeout(() => {  board = new five.Board({repl: false /*port: new EtherPort(3030)*/}) }, 5000)
       board.on('ready',() => {
         let relay = new five.Relay({ pin: 6, type: "NC" })
         relay.off()
-        // let touch = new five.Button({ pin: 9, holdtime: 5000 })
-
+    
         app.use('/abrir', (req, res, next) => {
           relay.on()
           var tempo = new Date()
           setTimeout(() => { relay.off() }, 1000)
           console.log("Aberto em: ",tempo)
-          res.sendStatus(200)
-          resolve()
+          res.send(200).json(tempo)
+          resolve(tempo)
           next()
         })
-        console.log('ok')
       })
-      if(!error){
-        resolve()
-      } else {
-        reject('Aconteceu um erro')
-      }
   })
 }
-arduino().then('OK').catch(err => console.log("ERRO DO CARALHO::::",err))
+arduino().then(console.log).catch(err => console.log(err))
 
 
 //---------------------------------------------//
